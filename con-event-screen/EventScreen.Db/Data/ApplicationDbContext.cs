c1fad288d9cbac8f309b3234ecee4a68789448c8
Demolite <tobi.brinke@gmail.com>
2023-08-08 21:03:49 +0200

    Started Rework

---
+++ con-event-screen/EventScreen.Db/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EventScreen.Db.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventScreen.Db.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+	}
+
+	public static ApplicationDbContext Get()
+	{
+		var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+					.UseSqlite("DataSource=app.db").Options;
+		return new ApplicationDbContext(options);
+	}
+	
+	public DbSet<ApplicationUser> Users { get; set; }
+}
