3fa58e3c1d9a9ca75746f7d50c66e6a596b56194
Demolite <tobi.brinke@gmail.com>
2023-08-09 21:58:10 +0200

    Dependency Manager

---
+++ con-event-screen/EventScreen.WebUi/DI/DependencyManager.cs
@@ -0,0 +1,22 @@
+﻿using EventScreen.Db.Models.Settings;
+using EventScreen.Db.Repositories;
+using EventScreen.Db.Repositories.Interfaces;
+using EventScreen.Services.Interfaces;
+using EventScreen.Services.Settings;
+using EventScreen.Services.Ui;
+
+namespace EventScreen.WebUi.DI;
+
+public static class DependencyManager
+{
+	public static void InjectServices(IServiceCollection collection)
+	{
+		collection.AddSingleton<IScreenSettingsRepository, ScreenSettingsRepository>();
+		collection.AddSingleton<IScreenRepository, ScreenRepository>();
+		collection.AddSingleton<IPagesService, PagesService>();
+		
+		collection.AddSingleton<IGlobalTimerService, GlobalTimerService>();
+
+		collection.AddSingleton<IPageManagerService, PageManagerService>();
+	}
+}
