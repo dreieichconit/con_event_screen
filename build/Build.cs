60bed99db5943ce0db397d69d81fa5d1ceff895d
Demolite <tobi.brinke@gmail.com>
2023-11-17 17:32:35 +0100

    Added Nuke

--- build/Build.cs
+++ build/Build.cs
@@ -1,108 +1,78 @@
-using System;
 using System.IO;
-using System.Linq;
-using DefaultNamespace;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
-	/// Support plugins are available for:
-	///   - JetBrains ReSharper        https://nuke.build/resharper
-	///   - JetBrains Rider            https://nuke.build/rider
-	///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-	///   - Microsoft VSCode           https://nuke.build/vscode
-	static DotNetPublishSettings pubSettings;
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
 
-	[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-	readonly Configuration Configuration = Configuration.Release;
+    [Solution] readonly Solution Solution;
+    AbsolutePath SourceDirectory => RootDirectory / "source";
+    AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath OutputDirectory => RootDirectory / "release";
+    
+    public static int Main () => Execute<Build>(x => x.Publish);
 
-	[Solution] readonly Solution Solution;
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = Configuration.Release;
 
-	AbsolutePath SourceDirectory => RootDirectory / "source";
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            foreach(var path in SourceDirectory.GlobDirectories("**/bin", "**/obj"))
+            {
+                path.DeleteDirectory();
+            }
+            
+            foreach(var path in TestsDirectory.GlobDirectories("**/bin", "**/obj"))
+            {
+                path.DeleteDirectory();
+            }
+            
+            OutputDirectory.CreateOrCleanDirectory();
+        });
 
-	AbsolutePath TestsDirectory => RootDirectory / "tests";
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
 
-	AbsolutePath OutputDirectory => RootDirectory / "release";
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                 .SetProjectFile(Solution)
+                 .SetConfiguration(Configuration)
+                 .EnableNoRestore()
+            );
+        });
 
-	public static int Main() => Execute<Build>(x => x.Upload);
+    Target Publish => _ => _
+         .DependsOn(Compile)
+         .Executes(() =>
+             {
+                 DotNetPublish(s => s
+                    .SetOutput(OutputDirectory)
+                    .SetConfiguration(Configuration.Release)
+                    .SetSelfContained(false)
+                    .SetPublishTrimmed(false)
+                 );
+             }
+         );
 
-	Target Clean => _ => _
-						.Before(Restore)
-						.Executes(() =>
-							{
-								foreach (var path in SourceDirectory.GlobDirectories("**/bin", "**/obj"))
-								{
-									path.DeleteDirectory();
-								}
-
-								foreach (var path in TestsDirectory.GlobDirectories("**/bin", "**/obj"))
-								{
-									path.DeleteDirectory();
-								}
-
-								OutputDirectory.CreateOrCleanDirectory();
-							}
-						);
-
-	Target Restore => _ => _
-							.DependsOn(Clean)
-							.Executes(() =>
-								{
-									DotNetRestore(s => s
-										.SetProjectFile(Solution)
-									);
-								}
-							);
-
-	Target Compile => _ => _
-							.DependsOn(Restore)
-							.Executes(() =>
-								{
-									DotNetBuild(s => s
-													.SetProjectFile(Solution)
-													.SetConfiguration(Configuration)
-													.EnableNoRestore()
-									);
-								}
-							);
-
-	Target Publish => _ => _
-							.DependsOn(Compile)
-							.Executes(() =>
-								{
-									DotNetPublish(s => s
-													.SetOutput(OutputDirectory)
-													.SetRuntime("ubuntu.18.04-x64")
-													.SetConfiguration(global::Configuration.Release)
-													.SetSelfContained(false)
-													.SetPublishTrimmed(false)
-									);
-
-									var rootPath = Path.Combine(Solution.Directory, "con-event-screen", "ApiDb.db");
-
-									CopyFile(rootPath, Path.Combine(rootPath, OutputDirectory, "ApiDb.db"),
-										FileExistsPolicy.Overwrite
-									);
-								}
-							);
-	
-	Target Upload => _ => _
-						.DependsOn(Publish)
-						.Executes(() =>
-						{
-							UploadHelper.UploadBuild(OutputDirectory);
-							UploadHelper.RestartApp();
-						});
 }
